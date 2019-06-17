4ae2eba5f6643065ca0b4b49b6ca507abd59fa48
Yidir <yader.dev@gmail.com>
2019-06-17 21:06:12 +0100

    Added tables representation
    Signed-off-by: Yidir <yader.dev@gmail.com>

--- src/app/owner/manage-restaurant/tabs/manage-tables/manage-tables.component.ts
+++ src/app/owner/manage-restaurant/tabs/manage-tables/manage-tables.component.ts
@@ -3,6 +3,7 @@ import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {AddTableComponent} from '../add-table/add-table.component';
 import {EditTableComponent} from '../edit-table/edit-table.component';
 import {IRestaurant} from '../../../../user/client/restaurant/restaurant.model';
+import {ITable} from '../../table.model';
 
 @Component({
   selector: 'app-manage-tables',
@@ -11,19 +12,41 @@ import {IRestaurant} from '../../../../user/client/restaurant/restaurant.model';
 })
 export class ManageTablesComponent implements OnInit {
   @Input() restaurant: IRestaurant;
+  tables: ITable[];
+  tablesRowOne: ITable[] = [];
+  tablesRowTwo: ITable[] = [];
 
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal) {
+  }
 
   ngOnInit() {
+    this.tables = this.restaurant.tables;
+    this.getTablesInRows();
   }
 
   openAddTable() {
-    console.log(this.restaurant);
+    console.log(this.restaurant.tables);
     const addTableModel = this.modalService.open(AddTableComponent);
     addTableModel.componentInstance.restaurant = this.restaurant;
   }
+
   openEditTable() {
     this.modalService.open(EditTableComponent);
   }
 
+  getTablesInRows() {
+    if (this.tables.length < 4) {
+      this.tablesRowOne = this.tables;
+    } else {
+      const halfCount = Math.floor(this.tables.length / 2);
+      this.tables.forEach((table) => {
+        if (this.tablesRowOne.length < halfCount) {
+          this.tablesRowOne.push(table);
+        } else {
+          this.tablesRowTwo.push(table);
+        }
+      });
+    }
+  }
 }
+
--- src/app/user/client/restaurant/restaurant.model.ts
+++ src/app/user/client/restaurant/restaurant.model.ts
@@ -1,7 +1,10 @@
+import {ITable} from '../../../owner/manage-restaurant/table.model';
+
 export interface IRestaurant {
   'id': number;
   'name': string;
   'address': string;
   'class': number;
   'updated_at': string;
+  'tables': ITable[];
 }
