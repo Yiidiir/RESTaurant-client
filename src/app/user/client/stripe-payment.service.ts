f6221bf9ff2c1ea6f56d57cc31a1fdabe95c9ed8
Yidir <yader.dev@gmail.com>
2019-06-30 22:41:55 +0100

    Added stripe payment service
    Signed-off-by: Yidir <yader.dev@gmail.com>

---
+++ src/app/user/client/stripe-payment.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {IOrder} from './my-orders/order.model';
+import {tap} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
+import {AuthService} from '../auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StripePaymentService {
+
+  constructor(private http: HttpClient, private auth: AuthService) {
+  }
+
+  generateTransaction(transaction, orderId: number) {
+    return this.http.post('/api/orders/' + orderId, transaction, this.auth.getHttpHeadersWithToken()).pipe(tap(data => {
+    }));
+  }
+
+}
